fcd06f884ed2b28fb329171484bb41834e349acf
waleed <waleed@4eyes.ch>
2017-09-22 15:27:49 +0200

    #4: code optimization

--- admin/public_html/php/athleteAllCompetitions.php
+++ admin/public_html/php/athleteAllCompetitions.php
@@ -1,23 +1,23 @@
 <?php
-session_start();
+    session_start();
 
-if ($_SESSION['eingeloggt'] == false) {
+    if ($_SESSION['eingeloggt'] == false) {
 
-    header("Location: public_html/index.php");
-    exit();
-}
+        header("Location: public_html/index.php");
+        exit();
+    }
 
-$athleteID = $_SESSION['athlete_id'];
-include 'Database.php';
-$pdo = Database::connect();
+    $athleteID = $_SESSION['athlete_id'];
+    include 'Database.php';
+    $pdo = Database::connect();
 
-$sql = "select * from tbl_competition";
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$q = $pdo->prepare($sql);
-$q->execute();
+    $sql = "select * from tbl_competition";
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $q = $pdo->prepare($sql);
+    $q->execute();
 
-Database::disconnect();
-?>
+    Database::disconnect();
+    ?>
 
 <!doctype html>
 <html lang="en">
@@ -218,57 +218,55 @@ Database::disconnect();
                 </footer>
             </div>
         </div>
+
+        <!--   Core JS Files   -->
+        <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
+        <script src="js/jquery-ui.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/material.min.js" type="text/javascript"></script>
+        <script src="js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
+        <!-- Forms Validations Plugin -->
+        <script src="js/jquery.validate.min.js"></script>
+        <!--  Plugin for Date Time Picker and Full Calendar Plugin-->
+        <script src="js/moment.min.js"></script>
+        <!--  Charts Plugin -->
+        <script src="js/chartist.min.js"></script>
+        <!--  Plugin for the Wizard -->
+        <script src="js/jquery.bootstrap-wizard.js"></script>
+        <!--  Notifications Plugin    -->
+        <script src="js/bootstrap-notify.js"></script>
+        <!-- DateTimePicker Plugin -->
+        <script src="js/bootstrap-datetimepicker.js"></script>
+        <!-- Vector Map plugin -->
+        <script src="js/jquery-jvectormap.js"></script>
+        <!-- Sliders Plugin -->
+        <script src="js/nouislider.min.js"></script>
+        <!--  Google Maps Plugin    -->
+        <script src="https://maps.googleapis.com/maps/api/js"></script>
+        <!-- Select Plugin -->
+        <script src="js/jquery.select-bootstrap.js"></script>
+        <!--  DataTables.net Plugin    -->
+        <script src="js/jquery.datatables.js"></script>
+        <!-- Sweet Alert 2 plugin -->
+        <script src="js/sweetalert2.js"></script>
+        <!--	Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+        <script src="js/jasny-bootstrap.min.js"></script>
+        <!--  Full Calendar Plugin    -->
+        <script src="js/fullcalendar.min.js"></script>
+        <!-- TagsInput Plugin -->
+        <script src="js/jquery.tagsinput.js"></script>
+        <!-- Material Dashboard javascript methods -->
+        <script src="js/material-dashboard.js"></script>
+        <!-- Material Dashboard DEMO methods, don't include it in your project! -->
+        <script src="js/demo.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function () {
+
+                // Javascript method's body can be found in assets/js/demos.js
+                demo.initDashboardPageCharts();
+
+                demo.initVectorMap();
+            });
+        </script>
     </body>
-    <!--   Core JS Files   -->
-    <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
-    <script src="js/jquery-ui.min.js" type="text/javascript"></script>
-    <script src="js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="js/material.min.js" type="text/javascript"></script>
-    <script src="js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
-    <!-- Forms Validations Plugin -->
-    <script src="js/jquery.validate.min.js"></script>
-    <!--  Plugin for Date Time Picker and Full Calendar Plugin-->
-    <script src="js/moment.min.js"></script>
-    <!--  Charts Plugin -->
-    <script src="js/chartist.min.js"></script>
-    <!--  Plugin for the Wizard -->
-    <script src="js/jquery.bootstrap-wizard.js"></script>
-    <!--  Notifications Plugin    -->
-    <script src="js/bootstrap-notify.js"></script>
-    <!-- DateTimePicker Plugin -->
-    <script src="js/bootstrap-datetimepicker.js"></script>
-    <!-- Vector Map plugin -->
-    <script src="js/jquery-jvectormap.js"></script>
-    <!-- Sliders Plugin -->
-    <script src="js/nouislider.min.js"></script>
-    <!--  Google Maps Plugin    -->
-    <script src="https://maps.googleapis.com/maps/api/js"></script>
-    <!-- Select Plugin -->
-    <script src="js/jquery.select-bootstrap.js"></script>
-    <!--  DataTables.net Plugin    -->
-    <script src="js/jquery.datatables.js"></script>
-    <!-- Sweet Alert 2 plugin -->
-    <script src="js/sweetalert2.js"></script>
-    <!--	Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
-    <script src="js/jasny-bootstrap.min.js"></script>
-    <!--  Full Calendar Plugin    -->
-    <script src="js/fullcalendar.min.js"></script>
-    <!-- TagsInput Plugin -->
-    <script src="js/jquery.tagsinput.js"></script>
-    <!-- Material Dashboard javascript methods -->
-    <script src="js/material-dashboard.js"></script>
-    <!-- Material Dashboard DEMO methods, don't include it in your project! -->
-    <script src="js/demo.js"></script>
-    <script type="text/javascript">
-                                $(document).ready(function () {
-
-                                    // Javascript method's body can be found in assets/js/demos.js
-                                    demo.initDashboardPageCharts();
-
-                                    demo.initVectorMap();
-                                });
-    </script>
-
-
-    
 </html>
